99ffb1a152469e926fe3b257d6bc30fa9eaa0729
Miguel Pajares <miguel.pajares@outlook.com>
2019-03-19 20:55:52 +0100

    Instanciación de productos e información por consola

--- MiguelPajaresBarcode/src/main/java/ufv/dis/parcial/MiguelPajaresBarcode/App.java
+++ MiguelPajaresBarcode/src/main/java/ufv/dis/parcial/MiguelPajaresBarcode/App.java
@@ -1,13 +1,31 @@
 package ufv.dis.parcial.MiguelPajaresBarcode;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	ArrayList<Producto> productos = new ArrayList<Producto>();
+    	
+    	String EAN13 = "978020537962";
+    	
+    	Producto producto1 = new Producto("Leche", "Asturiana", EAN13);
+    	Producto producto2 = new Producto("Cacao", "ColaCao", EAN13);
+    	Producto producto3 = new Producto("Refresco", "Fanta", EAN13);
+    	Producto producto4 = new Producto("Pizza", "Tarradellas", EAN13);
+    	
+    	productos.add(producto1);
+        productos.add(producto2);
+        productos.add(producto3);
+        productos.add(producto4);
+        
+        for(Producto a : productos) {
+     	   
+            System.out.println(a);
+
+        }
+
     }
 }
--- MiguelPajaresBarcode/src/main/java/ufv/dis/parcial/MiguelPajaresBarcode/Producto.java
+++ MiguelPajaresBarcode/src/main/java/ufv/dis/parcial/MiguelPajaresBarcode/Producto.java
@@ -1,10 +1,48 @@
 package ufv.dis.parcial.MiguelPajaresBarcode;
 
-import com.google.zxing;
 
 public class Producto {
 
 	private String nombre;
 	private String marca;
 	private String EAN13;
+	
+	public Producto(String nombre, String marca, String EAN13) {
+		super();
+		this.nombre = nombre;
+		this.marca = marca;
+		this.EAN13 = EAN13;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getEAN13() {
+		return EAN13;
+	}
+
+	public void setEAN13(String eAN13) {
+		EAN13 = eAN13;
+	}
+
+	@Override
+	public String toString() {
+		return "Nombre: " + this.getNombre() +
+				" Marca: " + this.getMarca() +
+				" EAN13: " + this.getEAN13();
+	}
+	
 }
